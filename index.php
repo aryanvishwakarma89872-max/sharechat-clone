251a87e49405a533e503f6464685ed8a5d1ad113
aryanvishwakarma89872-max <aryanvishwakarma89872@gmail.com>
2026-05-14 09:06:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -188,10 +188,14 @@ Get OTP
 </div>
 
 <!-- OR Line -->
-<div style="display:flex; align-items:center; margin:26px 0;">
-    <div style="width:150px; height:1px; background:#ccc;"></div>
-    <div style="padding:0 15px; font-size:20px; font-weight:bold;">Or</div>
-    <div style="flex:1; height:1px; background:#bdbdbd;"></div>
+<div style="display:flex; align-items:center; width:92%; margin:auto;">
+    
+    <div style="flex:1; height:1px; background:#ccc;"></div>
+
+    <div style="padding:0 15px; font-weight:bold;">Or</div>
+
+    <div style="flex:1; height:1px; background:#ccc;"></div>
+
 </div>
 
 <!-- Google Button -->
