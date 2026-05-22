44b105b3621bf8a9ebd6ee3ed5047d866137f68f
aryanvishwakarma89872-max <aryanvishwakarma89872@gmail.com>
2026-05-22 15:11:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -165,7 +165,30 @@ button{
 <div class="input-box">
 
     <div class="country">
-        🇮🇳 +91
+    <select id="countryCode">
+        <option value="+91">🇮🇳 +91</option>
+        <option value="+1">🇺🇸 +1</option>
+        <option value="+44">🇬🇧 +44</option>
+        <option value="+971">🇦🇪 +971</option>
+        <option value="+61">🇦🇺 +61</option>
+        <option value="+880">🇧🇩 +880</option>
+        <option value="+975">🇧🇹 +975</option>
+        <option value="+55">🇧🇷 +55</option>
+        <option value="+86">🇨🇳 +86</option>
+        <option value="+49">🇩🇪 +49</option>
+        <option value="+33">🇫🇷 +33</option>
+        <option value="+39">🇮🇹 +39</option>
+        <option value="+81">🇯🇵 +81</option>
+        <option value="+60">🇲🇾 +60</option>
+        <option value="+977">🇳🇵 +977</option>
+        <option value="+92">🇵🇰 +92</option>
+        <option value="+7">🇷🇺 +7</option>
+        <option value="+966">🇸🇦 +966</option>
+        <option value="+65">🇸🇬 +65</option>
+        <option value="+94">🇱🇰 +94</option>
+        <option value="+41">🇨🇭 +41</option>
+        <option value="+90">🇹🇷 +90</option>
+    </select>
     </div>
 
     <div class="phone">
