0de49e57f66f37b389958520003081012c091bbb
aryanvishwakarma89872-max <aryanvishwakarma89872@gmail.com>
2026-05-29 16:06:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -577,7 +577,7 @@ btn.disabled = true;
 function checkOTP() {
 
 let otp = document.getElementById("otp").value;
-let btn = document.getElementById("Confirm");
+let btn = document.getElementById("verifyBtn");
 
 /* only numbers */
 otp = otp.replace(/\D/g,'');
