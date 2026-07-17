8f497b6ac34b835ab5933fdf02f1e63968a368bc
aryanvishwakarma89872-max <aryanvishwakarma89872@gmail.com>
2026-07-17 13:57:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -638,6 +638,7 @@ document.querySelector(".logo").style.display = "none";
 document.getElementById("welcomeText").style.display = "none";
 document.getElementById("welcomeSubText").style.display = "none";
 document.getElementById("socialSection").style.display = "none";
+document.querySelector(".container").style.marginTop = "-80px";
 
 document.querySelector(".container").style.marginTop = "0px";
 
