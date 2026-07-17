de62017e0e71ad299798fcd78a66996557ce57ae
aryanvishwakarma89872-max <aryanvishwakarma89872@gmail.com>
2026-07-17 10:08:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -565,8 +565,8 @@ document.getElementById("attemptText").style.display = "block";
 
 document.getElementById("socialSection").style.display = "none";
 document.getElementById("logoSection").style.display = "none";
-document.querySelector(".input-box").style.marginTop = "-80px";
-document.getElementById("otpSection").style.marginTop = "-20px";
+document.querySelector(".input-box").style.marginTop = "20px";
+document.getElementById("otpSection").style.marginTop = "10px";
 
     
 /* OTP section ko upar lana */
